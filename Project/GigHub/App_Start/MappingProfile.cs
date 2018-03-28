39787b944b7750f24fa68e56f7ef75b3c69d29c1
radostina.z <radostina.z@sbtech.com>
2018-03-28 12:29:23 +0300

    Initial commit

---
+++ Project/GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GigHub.Controllers.Api;
+using GigHub.Models;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ApplicationUser, UserDto>();
+            CreateMap<Gig, GigDto>();
+            CreateMap<Notification, NotificationDto>();
+        }
+
+    }
+}
